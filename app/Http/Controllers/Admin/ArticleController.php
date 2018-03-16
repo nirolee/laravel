7d85f55c76dfd32730b091e61833e59c8de8dabf
niro <nirolee@foxmail.com>
2018-03-16 17:53:34 +0800

    3

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -9,6 +9,25 @@ class ArticleController extends Controller
 {
     //
     public function index(){
-      return view('admin/article/index')->withArticles(Article::all());
+        return view('admin/article/index')->withArticles(Article::all());
     }
+    public function create(){
+        return view('admin/article/create');
+    }
+    public function store(Request $request){
+        $this->validate($request, [
+            'title' => 'required|unique:articles|max:255',
+            'body' => 'required',
+        ]);
+        $article = new Article;
+        $article->title = $request->get('title');
+        $article->body = $request->get('body');
+        $article->user_id = $request->user()->id;
+        if($article->save()){
+            return redirect('admin/articles');
+        } else {
+            return redirect()->back()->withInput()->withErrors('报错失败');
+        }
+    }
+    
 }
